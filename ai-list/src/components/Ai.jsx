35d92eda606fa0c1fdd4f37f8ca567e82b1d4b6c
mabuhanifa <moh.abuhanifa@gmail.com>
2025-03-22 18:14:42 +0600

    bg & grid updated

--- ai-list/src/components/Ai.jsx
+++ ai-list/src/components/Ai.jsx
@@ -1,11 +1,11 @@
 export default function Ai({ name, logo, link }) {
   return (
-    <div className="p-3 border-1 rounded-lg border-gray-200 shadow-white">
+    <div className="p-3 border-1 rounded-lg border-gray-200 shadow-white bg-[#e6e6e6]">
       <div>
         <h2 className="text-xl font-medium">{name}</h2>
         <img src={logo} alt="Ai" className="w-20 my-2" />
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full cursor-alias"
+          className="bg-blue-600 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded w-full cursor-pointer"
           onClick={() => {
             window.open(link || "https://chat.openai.com/", "_blank");
           }}
